c74bafe2731f191ee287615ca4670ddeca04befc
songyongdong-git <syd1207@163.com>
2017-10-07 08:22:38 +0800

    通过职责链实现金融信息获取功能

--- src/guess_fortune.py
+++ src/guess_fortune.py
@@ -3,9 +3,14 @@ from price_hexun import Price_HeXun
 
 if __name__ == '__main__':
     BOC_price = Price_BOC()
+    hexun_price = Price_HeXun()
+
+    BOC_price.SetNextPriceObject(hexun_price)
+
     print(BOC_price.getExChangeRate("DOLLAR"))
     print(BOC_price.getMetalPrice("GOLD"))
 
-    hexun_price = Price_HeXun()
-    print(hexun_price.getExChangeRate("DOLLAR"))
-    print(hexun_price.getMetalPrice("GOLD"))
+    #print(hexun_price.getExChangeRate("DOLLAR"))
+    #print(hexun_price.getMetalPrice("GOLD"))
+
+    
--- src/price_object.py
+++ src/price_object.py
@@ -1,7 +1,10 @@
-#!获取中国银行金融信息
+#!获取金融信息
 class Price_Object:
+    #金融数据
     fGoldPrice = -1
     fDollarRate = -1
+    #下一个类(职责链)
+    classPriceNext = None
 
     def __init__(self):
         self.fGoldPrice = -1
@@ -9,8 +12,18 @@ class Price_Object:
 
     #!获取最新美元汇率
     def getExChangeRate(self, moneyType):
-        return self.fDollarRate
+        if self.fDollarRate == -1 and self.classPriceNext != None:
+            return self.classPriceNext.getExChangeRate(moneyType)
+        else:
+            return self.fDollarRate
 
     #!获取最新贵金属价格
     def getMetalPrice(self, metalType):
-        return self.fGoldPrice
+        if self.fGoldPrice == -1 and self.classPriceNext != None:
+            return self.classPriceNext.getMetalPrice(metalType)
+        else:
+            return self.fGoldPrice
+
+    #!设置职责链的下一个指向类
+    def SetNextPriceObject(self, price_object):
+        self.classPriceNext = price_object
